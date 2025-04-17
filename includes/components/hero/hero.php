1ddc486b1af10d9a95b8c3529479cd89d8f09288
Qasid Saleh <qasid.developer@gmail.com>
2025-04-17 13:58:15 -0400

    updated few variables and hero style

--- includes/components/hero/hero.php
+++ includes/components/hero/hero.php
@@ -12,27 +12,25 @@
 ?>
 
 <section id="hero" class="hero">
-    <div class="container-fluid">
-        <div class="section-inner">
-            <?php 
-                if($hero_image){
-                    echo '<div class="desktop-image'.$hide_desktop_image_for_mobile.'">';
-                    display_image($hero_image);
-                    echo '</div>';
-                } 
-                if($hero_mobile_image){
-                    echo '<div class="mobile-image'.$hide_mobile_image_for_desktop.'">';
-                    display_image($hero_mobile_image);
-                    echo '</div>';
-                } 
-            ?>
-            <div class="hero-caption">
-                <?php if($heading){ 
-                    echo '<h1>'.$heading.'</h1>';
-                } if($content){ 
-                    echo '<p>'.$content.'</p>';
-                } ?>
-            </div>
+    <div class="section-inner">
+        <?php 
+            if($hero_image){
+                echo '<div class="desktop-image '.$hide_desktop_image_for_mobile.'">';
+                display_image($hero_image);
+                echo '</div>';
+            } 
+            if($hero_mobile_image){
+                echo '<div class="mobile-image '.$hide_mobile_image_for_desktop.'">';
+                display_image($hero_mobile_image);
+                echo '</div>';
+            } 
+        ?>
+        <div class="hero-caption">
+            <?php if($heading){ 
+                echo '<h1>'.$heading.'</h1>';
+            } if($content){ 
+                echo '<p>'.$content.'</p>';
+            } ?>
         </div>
     </div>
 </section>
